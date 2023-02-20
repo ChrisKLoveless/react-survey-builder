34f55b49b14a8061e48098e6c9b12225878acb0a
ChrisKLoveless <chriskloveless@gmail.com>
2023-02-20 10:40:04 -0800

    connect firebase and firestore db

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import './App.css';
+import '../App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      It's working 
+      <Header />
+      <hr/>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
   return (
     <div className="header">
-      
+      <h1>Survey Builder</h1>
     </div>
   )
 }
